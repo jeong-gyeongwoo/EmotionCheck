fee9ca304f43c66e623bb57fe5baab564443c94e
gyeong <okbamtol@naver.com>
2023-07-30 18:17:44 +0900

    1차 과제

--- EmotionCheck/ViewController.swift
+++ EmotionCheck/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  
+    
+    
+    
+    
+    
+    enum emotion: Int {
+        case veryHappy, happy, notBad, bad, veryBad
+    }
+    //열거형 활용방법을 모르겠음....
+    // 글자를 양옆에? 레이블 2개?
+  
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func emotionButtonClicked(_ sender: UIButton) {
+        sender.tag += 1
+        print(sender.tag)
+        
+    }
+    
 }
 
